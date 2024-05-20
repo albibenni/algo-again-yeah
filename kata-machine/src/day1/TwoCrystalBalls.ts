7fd984649c324b6ef5c75ff7b9d899f28ab40709
Alberto Benatti <albi.benni8@gmail.com>
2024-05-20 20:29:26 +0200

    Update TwoCrystalBalls.ts

--- kata-machine/src/day1/TwoCrystalBalls.ts
+++ kata-machine/src/day1/TwoCrystalBalls.ts
@@ -3,18 +3,19 @@ export default function two_crystal_balls(breaks: boolean[]): number {
 
     let i = jmpAmount;
 
-    for(; i< breaks.length; i+= jmpAmount){
-        if(breaks[o]){
+    for (; i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
             break;
         }
     }
+
     i -= jmpAmount;
 
-    for (let j= 0 ; j <= jmpAmount && i < breaks.length; ++j){
-        if(break[i]){
+    for (let j = 0; j <= jmpAmount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
             return i;
         }
-    }   
+    }
     return -1
 
 }
