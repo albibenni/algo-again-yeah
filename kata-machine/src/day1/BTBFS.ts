0e0b8eaf7378ddd74921d05d93103c3e7ebbd210
Alberto Benatti <albi.benni8@gmail.com>
2024-05-22 18:39:12 +0200

    bfs with implemented queue

--- kata-machine/src/day1/BTBFS.ts
+++ kata-machine/src/day1/BTBFS.ts
@@ -1,3 +1,24 @@
+import Queue from "./Queue";
+
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
+    //const q: (BinaryNode<number> | null)[] = [head];
+    const q: Queue<BinaryNode<number> | null> = new Queue();
+    q.enqueue(head);
+
+    while (q.length) {
+        //const curr = q.shift() as BinaryNode<number> | undefined | null;
+        const curr = q.deque() as BinaryNode<number> | undefined | null;
+        if (!curr) continue;
+
+        // search
+        if (curr?.value === needle) {
+            return true;
+        }
+        //q.push(curr.left);
+        q.enqueue(curr.left);
+        //q.push(curr.right);
+        q.enqueue(curr.right);
+    }
+    return false
 }
