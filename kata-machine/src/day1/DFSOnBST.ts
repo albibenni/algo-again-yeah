3b312dbac86cd1afd23e268cb6cdfeceec8f7bb9
Alberto Benatti <albi.benni8@gmail.com>
2024-05-22 21:47:59 +0200

    binary search tree

--- kata-machine/src/day1/DFSOnBST.ts
+++ kata-machine/src/day1/DFSOnBST.ts
@@ -1,3 +1,18 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function seach(curr: BinaryNode<number> | null, needle: number): boolean {
+    if (!curr) {
+        return false;
+    }
 
+    if (curr.value === needle) {
+        return true;
+    }
+
+    if (curr.value < needle) {
+        return seach(curr.right, needle);
+    }
+    return seach(curr.left, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return seach(head, needle);
 }
